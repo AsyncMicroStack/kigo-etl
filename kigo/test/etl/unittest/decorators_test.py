5a5dd223f635b102cf9662f894c4d78631a867d7
MATEUSZ <m.kawa@op.pl>
2021-11-10 12:31:03 +0100

    implemented cleaning up and more tests

--- kigo/test/etl/unittest/decorators_test.py
+++ kigo/test/etl/unittest/decorators_test.py
@@ -9,6 +9,9 @@ from kigo.etl.file.readers import TextReader
 
 class MyTestCase(unittest.TestCase):
 
+    def setUp(self):
+        Config.load("resources/load.json")
+
     def test_proper_mapping(self):
         # GIVEN
         result = []
@@ -34,8 +37,64 @@ class MyTestCase(unittest.TestCase):
             result.append(str(MappingRegistry.mappings[mapping_key]))
 
         # THEN
+        self.assertListEqual(["MappingInfo <[<class 'kigo.test.etl.unittest.resources.entities.SomeClass'>, {}]> readers: [(<class 'kigo.etl.file.readers.TextReader'>, {'path': './data/input_1'})]>",
+                              "MappingInfo <[<class 'kigo.test.etl.unittest.resources.entities.SomeClass2'>, {}]> readers: [(<class 'kigo.etl.file.readers.TextReader'>, {'path': './data/input_2'})]>"],
+                             result)
+
+    @set_reader(TextReader, init_values={'path': './data/input_2'})
+    def test_proper_mapping_with_set_reader_without_config_load(self):
+        # GIVEN
+        result = []
+
+        # WHEN
+        for mapping_key in MappingRegistry.mappings:
+            result.append(str(MappingRegistry.mappings[mapping_key]))
+
+        # THEN
         self.assertListEqual(["MappingInfo <[<class 'kigo.test.etl.unittest.resources.entities.SomeClass'>, {}]> readers: [(<class 'kigo.etl.file.readers.TextReader'>, {'path': './data/input_2'})]>",
                               "MappingInfo <[<class 'kigo.test.etl.unittest.resources.entities.SomeClass2'>, {}]> readers: [(<class 'kigo.etl.file.readers.TextReader'>, {'path': './data/input_2'})]>"],
+                             result)
+
+    @set_reader(TextReader, init_values={'path': './data/input_2'}, mappings=kigo.test.etl.unittest.resources.entities.SomeClass)
+    def test_proper_mapping_with_set_reader_for_specific_entity_without_config_load(self):
+        # GIVEN
+        result = []
+
+        # WHEN
+        for mapping_key in MappingRegistry.mappings:
+            result.append(str(MappingRegistry.mappings[mapping_key]))
+
+        # THEN
+        self.assertListEqual(["MappingInfo <[<class 'kigo.test.etl.unittest.resources.entities.SomeClass'>, {}]> readers: [(<class 'kigo.etl.file.readers.TextReader'>, {'path': './data/input_2'})]>",
+                              "MappingInfo <[<class 'kigo.test.etl.unittest.resources.entities.SomeClass2'>, {}]> readers: [(<class 'kigo.etl.file.readers.TextReader'>, {'path': './data/input_1'})]>"],
+                             result)
+
+    @set_reader(TextReader, init_values={'path': './data/input_2'}, mappings=[kigo.test.etl.unittest.resources.entities.SomeClass])
+    def test_proper_mapping_with_set_reader_for_specific_entity_as_a_list_without_config_load(self):
+        # GIVEN
+        result = []
+
+        # WHEN
+        for mapping_key in MappingRegistry.mappings:
+            result.append(str(MappingRegistry.mappings[mapping_key]))
+
+        # THEN
+        self.assertListEqual(["MappingInfo <[<class 'kigo.test.etl.unittest.resources.entities.SomeClass'>, {}]> readers: [(<class 'kigo.etl.file.readers.TextReader'>, {'path': './data/input_2'})]>",
+                              "MappingInfo <[<class 'kigo.test.etl.unittest.resources.entities.SomeClass2'>, {}]> readers: [(<class 'kigo.etl.file.readers.TextReader'>, {'path': './data/input_1'})]>"],
+                             result)
+
+    def test_proper_mapping_without_setter_once_again(self):
+        # GIVEN
+        result = []
+
+        # WHEN
+        conf = Config.load("resources/load.json")
+        for mapping_key in MappingRegistry.mappings:
+            result.append(str(MappingRegistry.mappings[mapping_key]))
+
+        # THEN
+        self.assertListEqual(["MappingInfo <[<class 'kigo.test.etl.unittest.resources.entities.SomeClass'>, {}]> readers: [(<class 'kigo.etl.file.readers.TextReader'>, {'path': './data/input_1'})]>",
+                              "MappingInfo <[<class 'kigo.test.etl.unittest.resources.entities.SomeClass2'>, {}]> readers: [(<class 'kigo.etl.file.readers.TextReader'>, {'path': './data/input_1'})]>"],
                              result)
 
 
