2ddcc7e4236a16fd925ec99050dae70946a8e22f
MATEUSZ <m.kawa@op.pl>
2021-11-15 13:10:23 +0100

    removed load_mapping classmethod and added mapping property

--- kigo/etl/configuration.py
+++ kigo/etl/configuration.py
@@ -19,9 +19,9 @@ class Config:
     def config(self):
         return Config.__config
 
-    @classmethod
-    def load_mapping(cls):
-        pass
+    @property
+    def mapping(self):
+        return Mapping(Config.__config["mapping"])
 
     @classmethod
     def validate(cls):
